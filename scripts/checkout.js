0f0583ef5c3acd243e74c830c5898306814311e2
Abheeshta-P <softwarecheck01+github@gmail.com>
2024-06-30 18:56:58 +0530

    changes in file structure index.html rename

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 /*
 
@@ -11,3 +12,4 @@ Splits code into 3 parts
 */
 renderOrderSummary();
 
+renderPaymentSummary()
