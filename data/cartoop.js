db7cdb03f3bc1a39776568fb648499667fc8b671
Abheeshta-P <softwarecheck01+github@gmail.com>
2024-07-09 20:57:33 +0530

    single cart object using oop

---
+++ data/cartoop.js
@@ -0,0 +1,89 @@
+function Cart(){
+
+}
+
+const cart={
+  cartItems:undefined,
+  //loadFromStorage:function(){//dont use arrow fxn
+  loadFromStorage(){
+    this.cartItems=JSON.parse(localStorage.getItem('cart-oop'))
+  
+    //default value
+    if(!this.cartItems){
+      this.cartItems=[{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity:2,
+        deliveryOptionId:'1'
+      },{
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity:1,
+        deliveryOptionId:'2'
+      }];
+    }
+    
+  },
+  saveToLocalStorage(){
+    //adding to local storage
+    localStorage.setItem('cart-oop',JSON.stringify(this.cartItems)) 
+ },
+ addToCart(productId,quantity){
+
+  let matchingItem;//undifined is not found
+    this.cartItems.forEach((cartItem)=>{
+      if(cartItem.productId===productId)  
+        matchingItem=cartItem;
+    })
+    if(matchingItem)
+      matchingItem.quantity+=quantity;
+    else
+      this.cartItems.push({
+        productId,//destructuring property
+        quantity,
+        deliveryOptionId:'1'// default all are set to free delivery
+      });
+      this.saveToLocalStorage();
+     },
+
+     removeFromCart(productId){
+      const newCart=[];
+      
+      //create new cart except the element for which delete is clicked
+      this.cartItems.forEach((cartItem)=>{
+        if(cartItem.productId!==productId)
+          newCart.push(cartItem)
+        //newCart.push({productId:cartItem.productId,quantity:cartItem.quantity})
+      })
+      //change the cart
+      this.cartItems=newCart;
+      this.saveToLocalStorage();
+},
+updateCart(productId,quantity){  
+  //update the quantity of that item
+  this.cartItems.forEach((cartItem)=>{
+    if(cartItem.productId==productId){
+       cartItem.quantity=quantity;
+      }
+    })
+  this.saveToLocalStorage();
+},
+calculateCartQuantity(){
+  let quantity=0;
+  this.cartItems.forEach((cartItem)=>{
+    quantity+=cartItem.quantity;
+  })
+  if(quantity===0)
+    quantity=''
+  return quantity;
+},
+updateDeliveryOption(newOptionId,productId){
+  this.cartItems.forEach((cartItem)=>{
+    if(cartItem.productId==productId)
+      cartItem.deliveryOptionId=newOptionId;
+  })
+ this.saveToLocalStorage();
+}
+}
+cart.loadFromStorage()
+console.log(cart)
+
+
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-
+import "../data/cartoop.js"
 /*
 
 model view control : MVC (Design pattern) => These run in loop
