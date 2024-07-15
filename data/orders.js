ee20a4c5730f85c3542712d9ba6a4c2c1508e7c0
Abheeshta-P <softwarecheck01+github@gmail.com>
2024-07-15 22:14:56 +0530

    started with orders page js rendering

--- data/orders.js
+++ data/orders.js
@@ -1,5 +1,7 @@
-export const orders=JSON.parse(localStorage.getItem('orders'))||[]
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 
+
+export const orders=JSON.parse(localStorage.getItem('orders'))||[]
 
 export function addOrder(order){
   orders.unshift(order)
@@ -9,3 +11,12 @@ export function addOrder(order){
 function saveToStorage(){
   localStorage.setItem('orders',JSON.stringify(orders))
 }
+
+export function orderTimeCalculator(orderTime) {
+  const date = dayjs(orderTime);
+  const orderDay = date.date();
+  const orderMonth=date.format('MMMM');
+  return `${orderMonth} ${orderDay}`;
+}
+
+console.log(orderTimeCalculator('2024-07-15T15:43:12.216Z')) 
